517d404b467b16f9f7a5146412815ea846c05f24
aaamarkin <aaamarkin@gmail.com>
2019-06-21 16:49:53 +0300

    Initial commit

---
+++ GameTaskInitService.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using Common.GameTask;
+using UniRx;
+using UnityEngine.Assertions;
+
+namespace Common.Service
+{
+    /// <inheritdoc />
+    /// <summary>
+    /// Вспомогательная задача на инициализацию сервиса.
+    /// </summary>
+    public class GameTaskInitService : IGameTask
+    {
+        private readonly IGameService _gameService;
+        
+        public GameTaskInitService(IGameService gameService)
+        {
+            _gameService = gameService;
+        }
+        
+        // ITask
+        
+        public void Start()
+        {
+            Assert.IsFalse(_gameService.Ready.Value);
+            
+            IDisposable d = null;
+            d = _gameService.Ready.Subscribe(value =>
+            {
+                if (!value) return;
+                // ReSharper disable once AccessToModifiedClosure
+                d?.Dispose();
+                Complete.SetValueAndForceNotify(true);
+            });
+            _gameService.Initialize();
+        }
+
+        public void Reset(bool dispatchComplete = false)
+        {
+            throw new NotImplementedException();
+        }
+
+        IReadOnlyReactiveProperty<bool> IGameTask.Complete => Complete;
+        
+        // \ITask
+        
+        public BoolReactiveProperty Complete { get; } = new BoolReactiveProperty(false);
+    }
+}
